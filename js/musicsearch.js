f07a4c6e6a70e17d80fcd111fc83131826f37109
Sparklebunny Stevens <jeribeth@gmail.com>
2015-01-01 21:07:16 -0700

    Figured out API jsonp to return request

--- js/musicsearch.js
+++ js/musicsearch.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	$("#search-options").submit(function(event){
+	$("#submit-btn").click(function(event){
 		// zero out results if previous search has run
 		$("#results").html('');
 		// get the value of the input the user submitted
-		var artist = $(this).find("input[name='artist']").val();
+		var artist = $("input[name='artist']").val();
 		var genre = $(this).find("input[name='genre']").val();
 		var location = $(this).find("input[name='location']").val();
 		var dates = $(this).find("input[name='dates']").val();
 
 		var artistRequest = {api_key:"RRXS6RKJR0QZY7LZY",
 							 format:"jsonp",
-							 name:"radio",
-							 // bucket:"id:CAXFDYO12E2688C130",
-							 bucket:"id:7digital-US",
+							 name: artist,
+							 bucket:"id:CAXFDYO12E2688C130",
+							 // bucket:"id:7digital-US",
 							};
 
 		var result = $.ajax({
@@ -21,17 +21,15 @@ $(document).ready(function() {
 			url: "http://developer.echonest.com/api/v4/artist/search",
 			data: artistRequest,
 			dataType: "jsonp",
+			jsonpCallback: "MyJSFunc",
 			type: "GET",
 		}).done(function(result){
-			console.log("it works").append(result);
-			// var searchResults = showSearchResults(request.tagged, result.items.length);
+			console.log("it works" + result);
 
-			// $('.search-results').html(searchResults);
-
-			// $.each(result.items, function(i, item) {
-			// 	var question = showQuestion(item);
-			// 	$('.results').append(question);
-			// });
+// Refer to other API for code//
+			$.each(result.response.artists, function(i, item) {
+				$('#results').append("<div> item </div>"); 
+			});
 		}).fail(function(xhr, textStatus, error){
 			console.log(error);
 			// $('.search-results').append(errorElem);
